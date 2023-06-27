415bc7e1f13f7229a2325587d350385d88afa346
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-27 06:02:33 +0200

    Add funtion for fetching books from database and show it

--- books-and-genres-with-entity-framework/BooksAndGenresForm.cs
+++ books-and-genres-with-entity-framework/BooksAndGenresForm.cs
@@ -15,6 +15,21 @@ namespace books_and_genres_with_entity_framework
         public BooksAndGenresForm()
         {
             InitializeComponent();
+
+            dgvBooks.DataSource = getBooks();
+            dgvBooks.Columns[5].Visible = false;
+        }
+
+        private List<Books> getBooks()
+        {
+            List<Books> books = null;
+
+            using (var db = new EF_DBEntities())
+            {
+                books = db.Books.ToList();
+            }
+
+            return books;
         }
     }
 }
